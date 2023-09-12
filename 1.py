f183f67fb4df5f893f42513efd90579f84191c35
{seegong} <tw9138ho@gmail.com>
2023-09-12 10:44:24 +0900

    근하하하하하

--- 1.py
+++ 1.py
@@ -1,5 +1,25 @@
-import time
+import turtle 
+import tkinter
+import random
 
 
-print("hello")
-print("git aseasdads")
+def screenLeftclick(x, y) :
+    global r, g, b
+    turtle.pencolor ((r, g, b))
+    turtle.pendown()
+    turtle.goto(x,y)
+
+def screenRightclick(x, y) :
+    turtle.penup()
+    turtle.goto(x, y)
+
+def screenMidclick(x, y) :
+    global r, g, b
+    tSize = random.randrange(1, 10)
+    turtle.shapesize(tSize)
+    r = random.random()
+    g = random.random()
+    b = random.random()
+
+
+turtle.exitonclick()
---
+++ 4.py
@@ -0,0 +1,49 @@
+import turtle
+import tkinter
+import random
+
+a = 300
+_ = 300
+c = 3
+d = 0
+
+r,g,b,h,i,j,e = [0] * 7
+
+turtle.title('거북이 맘대로 싸돌아다니기')
+
+turtle.shape('turtle')
+turtle.pensize(c)
+turtle.setup(width= a + 30, height= _+30)
+turtle.screensize(a,_)
+
+
+while True :
+    r = random.random()
+    g = random.random()
+    b = random.random()
+    turtle.pencolor((r,g,b))
+
+
+    h = random.randrange(0,360)
+    i = random.randrange(1,100)
+    turtle.left(h)
+    turtle.forward(i)
+    j = turtle.xcor()
+    e = turtle.ycor()
+
+    if (-a / 2 <= j and j <= a/2) and (-_ / 2 <= e and e <=_ / 2):
+        pass
+    else :
+        turtle.penup
+        turtle.goto(0,0)
+        turtle.pendown
+
+        d += 1 
+        if d >= 5 :
+            break 
+
+turtle.done
+
+
+
+    
