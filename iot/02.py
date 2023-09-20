c6b6f377f977ef393c4b8124b08c20a34798c170
{seegong} <tw9138ho@gmail.com>
2023-09-20 11:25:35 +0900

    asdfasdfasdfg

--- iot/02.py
+++ iot/02.py
@@ -1,13 +1,15 @@
-import RPi.GPIO as GPIO 
-
-
-
-
-
+import RPi.GPIO as cc
 import time
-GPIO.setwarnings(False) 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(4, GPIO.OUT) 
-GPIO.output(4, GPIO.HIGH)
-GPIO.cleanup()
-asdf
+
+led = 4
+
+cc.setwarnings(False) 
+cc.setmode(cc.BCM) 
+
+for k in range(3):
+    cc.output(led,cc,1)    
+    time.sleep(2)
+    cc.output(led,cc,0)
+    time.sleep(2)
+
+cc.cleanup()
